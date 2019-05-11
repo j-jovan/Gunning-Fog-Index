d8aeb64f6b4905996df71200872e739e78e8042f
Jovan <abc.j.jovan@gmail.com>
2019-05-11 23:54:44 +0200

    Popravljeno par gresaka

--- Gunning_Fog_Index.py
+++ Gunning_Fog_Index.py
@@ -1,6 +1,7 @@
 #https://en.wikipedia.org/wiki/Gunning_fog_index
+from time import sleep
 
-while(ponovniUnos):
+while():
 	
 	unetTekst = input("Unesi tekst\n")
 
@@ -9,6 +10,10 @@ while(ponovniUnos):
 	brojReci = len(listaReci)
 	brojRecenica = len(unetTekst.split("."))-1
 	brojKompleksnihReci = 1 #Kompleksna rec je rec koja ima 3 ili vise vokala
+
+	if(brojRecenica == 0): #Privremeni fix
+		print("Sistem ne prepoznaje nijednu recenicu.")
+		break
 
 	for unetTekst in listaReci:
 		brojVokala = unetTekst.count('a') + unetTekst.count('e') + unetTekst.count('i')+ unetTekst.count('o')+ unetTekst.count('u') #Zbir vokala u svakoj unetoj reci
@@ -19,15 +24,15 @@ while(ponovniUnos):
 
 	print (gunningFogIndex)
 
-	opet = input("Da li želiš da uneses nov tekst? y/n\n").lower()
+	opet = input("Da li želiš da uneses nov tekst?\n").lower()
 
 	if(opet == "y" or "yes" or "da"): #Ako korisnik unese 'y', ponovi petlju
-	
-	elif(opet == "n" or "no" or "ne"): #Ako korisnik unese 'n', ponovi petlju
+		print("")
+	if(opet == "n" or "no" or "ne"): #Ako korisnik unese 'n', ponovi petlju
 		print("Doviđenja")
-		input("Pritisni bilo koje dugme za kraj.")
-		ponovniUnos = False
+		sleep(10)
+		break
 
 	else: #Ako korisnik ne izabere nista, ponovi petlju
-
+		print("")
 	
